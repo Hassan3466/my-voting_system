980a16be519c47ff5c810a28e37ba5e429dffcbb
Muaz Hassan <muzkhalifa01@gmail.com>
2025-11-12 18:23:59 +0100

    fix

--- voting/vite.config.js
+++ voting/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/my-voting_system",
   server: {
-  historyApiFallback: true
+  historyApiFallback: true  
   }
 })
