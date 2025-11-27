04c967542058b1c24a955d757884d74dbccb1d07
Muaz Hassan <muzkhalifa01@gmail.com>
2025-11-27 19:24:25 +0100

    latesting

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json({extended:true}))
 app.use(express.urlencoded({extended:true}))
 // app.use(cors({credentials:true, origin: ["http://localhost:/5173"]}))
-app.use(cors({ credentials: true, origin: ["http://localhost:5173"] }));
+app.use(cors({ credentials: true, origin: ["http://localhost:5173","http://vote.fuo.edu.ng", "https://vote.fuo.edu.ng"] }));
 app.use(fileUpload())
 
 
